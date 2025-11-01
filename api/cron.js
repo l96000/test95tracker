a713e25d73921ef7e3a596b393a099f383180ce8
l96000 <l64321608@gmail.com>
2025-11-01 13:27:27 +0100

    Update cron.js

--- api/cron.js
+++ api/cron.js
@@ -1,6 +1,6 @@
 // Fajl: api/cron.js (Koristi Google Sheets API)
 
-const { GoogleSpreadsheet } = await import('google-spreadsheet');
+import GoogleSpreadsheet from 'google-spreadsheet';
 // Nema potrebe za importom googleapis direktno, google-spreadsheet je dovoljan
 
 // --- Konekcija na Sheet ---
